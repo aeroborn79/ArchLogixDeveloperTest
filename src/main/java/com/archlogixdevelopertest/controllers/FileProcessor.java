cf24cb6b6952ee7e7bd18be347889b2e268fe818
aeroborn79 <aeroborn79@yahoo.com>
2018-02-02 20:41:48 -0600

    Ignore .csv first line fix

--- src/main/java/com/archlogixdevelopertest/controllers/FileProcessor.java
+++ src/main/java/com/archlogixdevelopertest/controllers/FileProcessor.java
@@ -19,31 +19,37 @@ public class FileProcessor {
 	    String startDate = PathUtils.getStartDate(path);
 	    
 	    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {	
-	        while ((line = br.readLine()) != null) {	
-	            String[] work = line.split(DELIMTER);
-	            employee.setEmail(work[0]);
-	            ArrayList<WorkDetail> workDetails = new ArrayList<WorkDetail>();
-	            
-	            for(int i = 1; i < work.length; i++) {
-	            	int hours = Integer.parseInt(work[i]);
-	            	String date = PathUtils.dateParser(startDate, "year") + "-"
-	            			+ PathUtils.dateParser(startDate, "month") + "-"
-	            			+ String.valueOf((Integer.parseInt(PathUtils.dateParser(startDate, "day")) + (i - 1)));
-	            	WorkDetail workDetail = new WorkDetail(date, hours);
-	            	workDetails.add(workDetail);
+	        int count = 0;
+	    	while ((line = br.readLine()) != null) {	
+	            if (count == 0) {
+	            	count++;
 	            }
-	            employee.setWork(workDetails);
-	            
-	            // Make api call to complete employee parameters
-	            String response = GetController.getEmployeeData(employee.getEmail());
-	            System.out.println(response);
-	            Gson gson = new Gson();
-	            Employee temp = gson.fromJson(response, Employee.class);
-	            System.out.println(temp.getFname() + " " + temp.getLname());
-	            employee.setId(temp.getId());
-	            temp = null;
-	            PostController.logHours(employee.getId(), employee.getWork());
-	            
+	            else if(count > 0) {
+		    		String[] work = line.split(DELIMTER);
+		            employee.setEmail(work[0]);
+		            ArrayList<WorkDetail> workDetails = new ArrayList<WorkDetail>();
+		            
+		            for(int i = 1; i < work.length; i++) {
+		            	int hours = Integer.parseInt(work[i]);
+		            	String date = PathUtils.dateParser(startDate, "year") + "-"
+		            			+ PathUtils.dateParser(startDate, "month") + "-"
+		            			+ String.valueOf((Integer.parseInt(PathUtils.dateParser(startDate, "day")) + (i - 1)));
+		            	WorkDetail workDetail = new WorkDetail(date, hours);
+		            	workDetails.add(workDetail);
+		            }
+		            employee.setWork(workDetails);
+		            
+		            // Make api call to complete employee parameters
+		            String response = GetController.getEmployeeData(employee.getEmail());
+		            System.out.println(response);
+		            Gson gson = new Gson();
+		            Employee temp = gson.fromJson(response, Employee.class);
+		            System.out.println(temp.getFname() + " " + temp.getLname());
+		            employee.setId(temp.getId());
+		            temp = null;
+		            PostController.logHours(employee.getId(), employee.getWork());
+		            count++;
+	            }
 	        }
 	        GetController.getEmployeeHours();
 	    } catch (IOException e) {
